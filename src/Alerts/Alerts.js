845483c548045607df885a7dbf64c8c19925e9dc
BAR Eran <eran.bar@hexagon.com>
2026-03-18 11:13:30 +0200

    fixed url

--- src/Alerts/Alerts.js
+++ src/Alerts/Alerts.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./Alerts.css";
 
-const ALERTS_URL = "http://localhost:4000/api/alerts";
+const ALERTS_URL = "https://lobby-display-sh6g.onrender.com/api/alerts";
 
 const Alerts = () => {
   const [alerts, setAlerts] = useState([]);
